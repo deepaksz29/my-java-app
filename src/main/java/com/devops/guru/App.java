1fc5c6fdda431a81795f249c7b4e3bcf8c8cb83e
deepaksz29 <silverzonefoundation@SilverZones-Mac-mini-3.local>
2026-01-14 10:34:13 +0530

    4th version update

--- src/main/java/com/devops/guru/App.java
+++ src/main/java/com/devops/guru/App.java
@@ -2,5 +2,5 @@ package com.devops.guru;
 public class App{
 public static void main(String []args)
 {
-System.out.println("DevOps Mastery: Version 3 is here!");
+System.out.println("DevOps Mastery: Version 4 Completed by Myself!");
 }}
