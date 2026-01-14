c19c109ed1becc03db5cdfedd441b88c62b18343
deepaksz29 <silverzonefoundation@SilverZones-Mac-mini-3.local>
2026-01-14 16:51:49 +0530

    Final Automation Test

--- src/main/java/com/devops/guru/App.java
+++ src/main/java/com/devops/guru/App.java
@@ -2,5 +2,5 @@ package com.devops.guru;
 public class App{
 public static void main(String []args)
 {
-System.out.println("DevOps Mastery: Version 4 Completed by Myself!");
+System.out.println("CONGRATULATIONS! Automation is 100% Success! V5");
 }}
